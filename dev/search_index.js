cadef655e04454178b0b7a11d85707926eaf1377
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-07-06 13:24:47 +0000

    build based on be58de0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"api/#API-Reference","page":"API Reference","title":"API Reference","text":"","category":"section"},{"location":"api/","page":"API Reference","title":"API Reference","text":"Modules = [ReduxImGui]\nOrder   = [:constant, :function, :type]","category":"page"},{"location":"#ReduxImGui","page":"Introduction","title":"ReduxImGui","text":"","category":"section"}]
+}
